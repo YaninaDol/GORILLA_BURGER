433e42a9d285df381f1b4fa8aa7b98995fdb1fbd
Yana Doll <Dolz_op79@student.itstep.org>
2022-12-06 21:51:23 +0400

    correct

--- Deliveroo/MenuForm.Designer.cs
+++ Deliveroo/MenuForm.Designer.cs
@@ -71,7 +71,7 @@ namespace Deliveroo
             this.panel1.BackColor = System.Drawing.Color.Black;
             this.panel1.Location = new System.Drawing.Point(243, 161);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(360, 556);
+            this.panel1.Size = new System.Drawing.Size(360, 563);
             this.panel1.TabIndex = 5;
             this.panel1.Click += new System.EventHandler(this.Panel1_Click);
             // 
