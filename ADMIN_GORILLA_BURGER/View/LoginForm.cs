0357e6eb94d99f8b6e1af628b069071ec8024036
Yana Doll <Dolz_op79@student.itstep.org>
2022-12-07 23:19:24 +0400

    setup files

--- ADMIN_GORILLA_BURGER/View/LoginForm.cs
+++ ADMIN_GORILLA_BURGER/View/LoginForm.cs
@@ -36,6 +36,7 @@ namespace ADMIN_GORILLA_BURGER
                     {
                         MessageBox.Show("Erorr Password");
                         this.materialSingleLineTextField2.Text = "";
+                      
                     }
                 }
                 else
